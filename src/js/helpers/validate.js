b38c545ac4fdde690dfbe2661039f52ff2d1d405
Daria Menscikova <menscikova.daria@gmail.com>
2020-07-06 14:40:20 +0200

    added config

---
+++ src/js/helpers/validate.js
@@ -0,0 +1,14 @@
+const regExpDic = {
+  email:  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+  password: /^[0-9a-zA-Z]{4,}$/,
+};
+
+/**
+ * Function validate. Check Input on RegExp provided in regExpDic by input data-required type
+ * @param {HTMLInputElement} el
+ * @returns {Boolean} - Return true if input valid or
+ */
+
+export function validate(el) {
+
+}
